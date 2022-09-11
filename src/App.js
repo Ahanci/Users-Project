337136221edaab2cdd042bb3e12a2436262a5bd3
Abdulkerim Hancıoglu <ahancimerve@gmail.com>
2022-09-11 18:53:21 +0300

    Staying login with localstorage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import './App.css';
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route,Navigate } from 'react-router-dom'
 import Layout from './components/Layout/Layout';
 import HomePage from './pages/HomePage';
 import Login from './pages/Login';
 import Users from './pages/Users';
+import { useContext } from 'react';
+import AuthContext from './store/auth-context';
 
 
 function App() {
- 
+  const authCtx = useContext(AuthContext);
+
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={ <HomePage/> } />
-        <Route path="/login" element={ <Login/> } />
-        <Route path="/users" element={ <Users/> } />
+        <Route path="/" element={<HomePage />} />
+
+        {!authCtx.isLoggedIn && (<Route path="/login" element={<Login />} />)}
+
+        {authCtx.isLoggedIn && (
+          <Route path="/users" element={<Users />} />
+        ) }
+        <Route  path="*" element={ <Navigate to="/" /> } />
+        
       </Routes>
     </Layout>
-   
+
   );
 }
 
